068c3fe39b348562352a2a12d42832769f02ded4
Pintu kumar <67854350+Pintu0504@users.noreply.github.com>
2021-03-02 22:46:32 +0530

    Create lib.js

---
+++ javascript/import in Nodejs/lib.js
@@ -0,0 +1,12 @@
+function awesomeFunction(){
+    console.log("Hi awesome Pintu kumar")
+}
+
+function badFunction(){
+    console.log("Hi Bad Pintu kumar")
+}
+
+module.exports={
+    awesomeFunction,
+    badFunction
+}
